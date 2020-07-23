07ef789a553509f8ef1bd0272f86aa496589071f
Christian Nunciato <c@nunciato.org>
2020-07-22 17:20:28 -0700

    Try without forcible clean

--- scripts/pulumify.sh
+++ scripts/pulumify.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-which node
-node -v
-
-which yarn
-yarn -v
-
-pwd
-
-yarn cache clean
-rm -rf node_modules
+# To be perfectly honest, I'm not yet sure why we have to do this, but we do. It's only required
+# for Pulumify builds, so it must have something to do with the way we're executing the
+# PULUMIFY BUILD command.
+# rm -rf node_modules
+# yarn cache clean
 yarn install
 
 ./scripts/build-site.sh preview
