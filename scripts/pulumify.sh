4b8d946387ea4d213ad50cd19fafa2362bbe099c
Christian Nunciato <c@nunciato.org>
2020-07-22 17:37:51 -0700

    Update some comments

--- scripts/pulumify.sh
+++ scripts/pulumify.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# To be perfectly honest, I'm not yet sure why we have to do this, but we do. It's only required
-# for Pulumify builds, so it must have something to do with the way we're executing the
-# PULUMIFY BUILD command.
+# To be perfectly honest, I'm not yet sure why we have to do this a second time here, but
+# we do. It's only required for Pulumify builds, so it must have something to do with how
+# we're executing the PULUMIFY BUILD command.
 # rm -rf node_modules
 # yarn cache clean
 yarn install
